301362cccd09fc6cb0933781a4fd3e92ee45bfbd
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2020-02-12 01:51:30 +0000

    sm7125-common: overlay: Enable freeform windows
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: kyvangka1610 <kyvangka2002@gmail.com>

--- common.mk
+++ common.mk
@@ -140,6 +140,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.capabilityconfigstore@1.0.vendor
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # GNSS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1.vendor
