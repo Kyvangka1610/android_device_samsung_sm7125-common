b15cc7fa40075e9b55febe94ea9f4d616da13f1c
Mathieu Chartier <mathieuc@google.com>
2021-08-22 16:31:23 +0800

    sm7125-common: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3
    Signed-off-by: Hazama25 <hazamafawkes@gmail.com>
    Signed-off-by: kyvangka1610 <kyvangka2002@gmail.com>

--- common.mk
+++ common.mk
@@ -468,7 +468,10 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/samsung \
     hardware/samsung/aidl/power-libperfmgr
-    
+
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Storage
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.fuse.passthrough.enable=true
