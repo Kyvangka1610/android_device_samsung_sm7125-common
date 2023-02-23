7176ec113964dd5f8531c07069cd89782e050be3
Pranav Vashi <neobuddy89@gmail.com>
2023-02-23 20:16:59 +0300

    sm7125-common: Build missing camera packages
    Signed-off-by: kyvangka1610 <kyvangka2002@gmail.com>

--- common.mk
+++ common.mk
@@ -121,6 +121,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     android.hardware.camera.device@3.6.vendor \
     android.hardware.camera.provider@2.6.vendor \
+    libcamera2ndk_vendor \
+    libstdc++.vendor \
     Camera2
 
 # CAS
