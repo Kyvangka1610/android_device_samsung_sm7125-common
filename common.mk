485a2bdcd2b4c877cc869ea2a28f159e1fd8b622
kyvangka1610 <kyvangka2002@gmail.com>
2023-07-15 08:40:36 +0700

    sm7125-common: move FUSE passthrough to system.prop
    Signed-off-by: kyvangka1610 <kyvangka2002@gmail.com>

--- common.mk
+++ common.mk
@@ -545,10 +545,6 @@ PRODUCT_SOONG_NAMESPACES += \
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
-# Storage
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.fuse.passthrough.enable=true
-
 # Prop files
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
