5c7a759ee6478c6788cb6bb95db45a9875e40cf0
Ruchit <risenid@duck.com>
2023-11-12 15:58:03 +0530

    sm7125-common: Pull camera blobs from A13
    Signed-off-by: kyvangka1610 <kyvangka2002@gmail.com>

--- common.mk
+++ common.mk
@@ -101,28 +101,9 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth@1.0.vendor
-
+    
 # Blur
 TARGET_ENABLE_BLUR := true
-
-# Camera
-PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.6-service.sm7125 \
-    android.hardware.camera.provider@2.4-legacy \
-    android.hardware.camera.provider@2.5-legacy \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
-    camera.device@3.3-impl \
-    camera.device@3.4-impl \
-    camera.device@3.5-impl \
-    libgrallocusage.vendor \
-    vendor.qti.hardware.camera.device@1.0.vendor \
-    android.hardware.camera.device@3.6.vendor \
-    android.hardware.camera.provider@2.6.vendor \
-    libcamera2ndk_vendor \
-    libdng_sdk.vendor \
-    vendor.qti.hardware.camera.postproc@1.0.vendor \
-    libstdc++_vendor 
 
 # CAS
 PRODUCT_PACKAGES += \
